1702391ba54d401a38dac5c9aab7c612615ee7ac
Mia1251 <svanteomax@gmail.com>
2021-02-25 12:46:50 +0100

    Part 1 done.

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,16 @@
+package se.lexicon;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import se.lexicon.config.ComponentScanConfig;
+import se.lexicon.data_access.StudentDao;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+
+        StudentDao studentDao = context.getBean(StudentDao.class);
+    }
+
+}
---
+++ src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
@@ -0,0 +1,29 @@
+package se.lexicon.data_access;
+
+import org.springframework.stereotype.Component;
+import se.lexicon.model.Student;
+
+import java.util.List;
+@Component
+public class StudentDaoListImpl implements StudentDao{
+
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student find(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
